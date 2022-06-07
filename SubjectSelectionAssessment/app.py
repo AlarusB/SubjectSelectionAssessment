ebe2e4507bf40a047dc478de14af90373bb31a59
Abdul Boateng <abdboateng@student.pakuranga.school.nz>
2022-06-08 09:55:57 +1200

    Added setup and utils so the website can connect to database

--- SubjectSelectionAssessment/app.py
+++ SubjectSelectionAssessment/app.py
@@ -1,13 +1,10 @@
-"""
-This script runs the application using a development server.
-It contains the definition of routes and views for the application.
-"""
-
-from flask import Flask
+import uuid, os, hashlib, pymysql
+from flask import Flask, request, render_template, redirect, url_for, session, abort, flash, jsonify
 app = Flask(__name__)
 
-# Make the WSGI interface available at the top level so wfastcgi can get it.
-wsgi_app = app.wsgi_app
+# Register the setup page and import create_connection()
+from utils import create_connection, setup
+app.register_blueprint(setup)
 
 
 @app.route('/')
@@ -17,9 +14,14 @@ def hello():
 
 if __name__ == '__main__':
     import os
+
+    # This is required to allow flashing messages. We will cover this later.
+    app.secret_key = os.urandom(32)
+
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT, debug = True)
+    app.run(HOST, PORT, debug=True)
+
